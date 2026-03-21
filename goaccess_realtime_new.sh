60ce78c64ea6370fccbfae64be5c15ead50c40b0
Abinash Dash <abinashdash17@gmail.com>
2026-03-21 13:14:51 +0530

    Update goaccess_realtime_new.sh

--- goaccess_realtime_new.sh
+++ goaccess_realtime_new.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 (zcat -f /var/log/nginx/access.log.*.gz /var/log/nginx/access.log.1 && tail -f /var/log/nginx/access.log) | goaccess - --log-format=COMBINED
+# aliases
+# alias show_access_logs='sudo goaccess /var/log/nginx/access.log --log-format=COMBINED'
+# alias show_rt_access_logs='(zcat -f /var/log/nginx/access.log.*.gz /var/log/nginx/access.log.1 && tail -f /var/log/nginx/access.log) | goaccess - --log-format=COMBINED'
